054cff9d3c2ca634ec0d148fc2768836423cad1f
ryan mcdaniel <mcdanielrp@users.noreply.github.com>
2015-07-26 10:37:58 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## author: r mcdaniel
+## date: 7-26-15
+##
 
-## Write a short comment describing this function
+
+## The function below sets a matrix 'x' to the cache
+## so it can then be accessed quickly later.
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y){
+        
+                x <<- y
+                m <<- NULL
+        
+        }
+        
+        ## setting up get,setmean,getmean
+        
+        get <- function () x
+        setmean <- function (mean) m <<- mean
+        getmean <- function () m
+        list(set = set, 
+                get = get,
+                setmean = setmean,
+                getmean = getmean)
+
 }
 
 
-## Write a short comment describing this function
+## This function extracts cached matrix defined in this environment
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmean()
+        if(!is.null(m)){
+        
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        m <- mean(data,...)
+        x$setmean(m)
 }
+
